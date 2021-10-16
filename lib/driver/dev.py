9794e5ec12357b9b72342d688f5ca4c7d5f54af2
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-10-16 11:28:02 +0700

    Create dev.py

---
+++ lib/driver/dev.py
@@ -0,0 +1,103 @@
+import io
+import sys
+import traceback
+import speedtest
+from lib.config import SUDO_USERS
+from pyrogram import Client, filters
+from pyrogram.errors import RPCError
+
+
+@Client.on_message(filters.command("eval") & filters.user(SUDO_USERS))
+async def eval(client, message):
+    status_message = await message.reply_text("Processing ...")
+    cmd = message.text.split(" ", maxsplit=1)[1]
+
+    reply_to_ = message
+    if message.reply_to_message:
+        reply_to_ = message.reply_to_message
+
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+
+    try:
+        await aexec(cmd, client, message)
+    except Exception:
+        exc = traceback.format_exc()
+
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success"
+
+    final_output = "<b>EVAL</b>: "
+    final_output += f"<code>{cmd}</code>\n\n"
+    final_output += "<b>OUTPUT</b>:\n"
+    final_output += f"<code>{evaluation.strip()}</code> \n"
+
+    if len(final_output) > 4096:
+        with io.BytesIO(str.encode(final_output)) as out_file:
+            out_file.name = "eval.text"
+            await reply_to_.reply_document(
+                document=out_file, caption=cmd, disable_notification=True
+            )
+    else:
+        await reply_to_.reply_text(final_output)
+    await status_message.delete()
+
+
+async def aexec(code, client, message):
+    exec(
+        "async def __aexec(client, message): "
+        + "".join(f"\n {l_}" for l_ in code.split("\n"))
+    )
+    return await locals()["__aexec"](client, message)
+
+
+
+@Client.on_message(filters.command("speedtest") & filters.user(SUDO_USERS))
+def speedtest_(_,message):
+    speed = speedtest.Speedtest()
+    speed.get_best_server()
+    speed.download()
+    speed.upload()
+    speedtest_image = speed.results.share()
+
+    message.reply_photo(speedtest_image)
+
+
+@Client.on_message(filters.command("leave") & filters.user(SUDO_USERS))
+async def leave(client, message):
+    if len(message.command) == 1:
+        try:
+            await client.leave_chat(message.chat.id)
+        except RPCError as e:
+            print(e)
+    else:
+        cmd = message.text.split(maxsplit=1)[1]
+        try:
+            await client.leave_chat(int(cmd))
+        except RPCError as e:
+            print(e)
+
+
+@Client.on_message(filters.command("invitelink"))
+async def invitelink(client, message):
+    chat_id = message.chat.id
+    try:
+        grouplink = await client.export_chat_invite_link(chat_id)
+        await message.reply_text(f"{grouplink}")
+    except Exception as e:
+        pass
