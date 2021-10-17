d20bf0da38e336a3f28c1cb8b99f4e472ac09d73
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-17 22:16:39 +0700

    Create callback.py

---
+++ lib/driver/callback.py
@@ -0,0 +1,119 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, Chat, CallbackQuery
+
+
+@Client.on_callback_query(filters.regex("cbhelp"))
+async def cbstart(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""Help menu with description, you can tap on button for info!""",
+        reply_markup=InlineKeyboardMarkup(
+            [ 
+                [
+                    InlineKeyboardButton(
+                        "▶️ Play", callback_data="cbplay")
+                    InlineKeyboardButton(
+                        "⏸️ Pause​​", callback_data="cbpause"
+                    ),
+                 ],
+                 [
+                    InlineKeyboardButton(
+                        "◀️ Resume", calback_data="cbresume")
+                    InlineKeyboardButton(
+                        "⏹️ Stop", callback_data="cbstop"
+                    )
+                 ]
+             ]
+         ),
+     )
+
+
+@Client.on_callback_query(filters.regex("cbplay"))
+async def cbplay(_, query: CallbackQuery):
+    await query.edit_message_text("""**[HELP MESSAGE]**
+
+**>> Description:** ```to streaming video in video chat group/channel```
+**[GUIDE]**
+**>> Group:** ```/play [reply to video/audio/give youtube url]```
+**>> Channel:** ```/play [channel] [reply to video/audio/give youtube url]```
+
+**>> Note:** ```To stream in channel stream you must replace chat title to Channel ID```""",
+        reply_markup=InlineKeyboardMarkup(
+            [ 
+                [
+                    InlineKeyboardButton(
+                        "🔙 Back", callback_data="cbhelp"
+                    )
+                ]
+            ]
+        ),
+    )
+
+
+@Client.on_callback_query(filters.regex("cbpause"))
+async def cbpause(_, query: CallbackQuery):
+    await query.edit_message_text("""**[HELP MESSAGE]**
+
+**>> Description:** ```To pause stream in video chat group/channel```
+
+**[GUIDE]**
+
+**>> Group:** ```/pause```
+**>> Channel:** ```/pause [channel]```
+
+**>> Note:** ```Replace chat title to pause channel stream```""",
+        reply_markup=InlineKeyboardMarkup(
+            [ 
+                [
+                    InlineKeyboardButton(
+                        "🔙 Back", callback_data="cbhelp"
+                    )
+                ]
+            ]
+        ),
+    )
+
+
+@Client.on_callback_query(filters.regex("cbresume"))
+async def cbresume(_, query: CallbackQuery):
+    await query.edit_message_text("""**[HELP MESSAGE]**
+
+**>> Description:** ```To resume stream in video chat grouo/channel```
+**[GUIDE]**
+**>> Group:** ```/resume```
+**>> Channel:** ```/resume [channel]```
+
+**>> Note:** ```Replace chat title to resume channel stream```""",
+        reply_markup=InlineKeyboardMarkup(
+            [ 
+                [
+                    InlineKeyboardButton(
+                        "🔙 Back", callback_data="cbhelp"
+                    )
+                ]
+            ]
+        ),
+    )
+
+
+@Client.on_callback_query(filters.regex("cbstop"))
+async def cbstop(_, query: CallbackQuery):
+    await query.edit_message_text("""**[HELP MESSAGE]**
+
+**>> Description:** ```To stop stream in video chat grouo/channel```
+**[GUIDE]**
+**>> Group:** ```/stop```
+**>> Channel:** ```/stop [channel]```
+
+**>> Note:** ```Replace chat title to stop channel stream```""",
+        reply_markup=InlineKeyboardMarkup(
+            [ 
+                [
+                    InlineKeyboardButton(
+                        "🔙 Back", callback_data="cbhelp"
+                    )
+                ]
+            ]
+        ),
+    )
+
+
