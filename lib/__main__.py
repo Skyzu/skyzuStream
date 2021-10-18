9938a1de29af0e635da4110298cdd9385faad2d0
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-10-19 05:17:44 +0700

    Update __main__.py

--- lib/__main__.py
+++ lib/__main__.py
@@ -1,4 +1,4 @@
-'''
+"""
 tg-stream-video, An Telegram Bot Project
 Copyright (c) 2021 GalihMrd <https://github.com/Imszy17>
 This program is free software: you can redistribute it and/or modify
@@ -11,12 +11,13 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
-'''
+"""
 
 from lib.tg_stream import app
 from pyrogram import Client, idle
 from lib.config import API_HASH, API_ID, BOT_TOKEN
 from lib.tg_stream import call_py
+
 
 bot = Client(
     ":memory:",
@@ -25,6 +26,7 @@ bot = Client(
     bot_token=BOT_TOKEN,
     plugins=dict(root="lib.driver"),
 )
+
 
 bot.start()
 app.start()
