c0d909c3f2c16bf4f505a9092d74cfd531c162dd
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-10-18 18:41:42 +0700

    Update config.py

--- lib/config.py
+++ lib/config.py
@@ -1,16 +1,10 @@
 import os
-from os import getenv
-from dotenv import load_dotenv
 
-if os.path.exists("local.env"):
-    load_dotenv("local.env")
-
-load_dotenv()
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
-SESSION_NAME = getenv("SESSION_NAME")
-USERNAME_BOT = getenv("USERNAME_BOT", "Feritapibot")
-OWNER_NAME = getenv("OWNER_NAME", "Xflicks")
-COMMAND_PREFIXES = getenv("COMMAND_PREFIXES", "!")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS"))
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SESSION_NAME = os.getenv("SESSION_NAME")
+USERNAME_BOT = os.getenv("USERNAME_BOT", "Feritapibot")
+OWNER_NAME = os.getenv("OWNER_NAME", "Xflicks")
+COMMAND_PREFIXES = os.getenv("COMMAND_PREFIXES", "!")
+SUDO_USERS = list(map(int, os.getenv("SUDO_USERS"))
