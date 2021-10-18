03b69690757ee0b944648565428c80484adcf6af
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-10-19 05:24:16 +0700

    Update dev.py

--- lib/driver/dev.py
+++ lib/driver/dev.py
@@ -18,8 +18,22 @@ import sys
 import traceback
 import speedtest
 from lib.helpers.decorators import authorized_users_only, sudo_users_only
+from pyrogram.types import Message
 from pyrogram import Client, filters
 from pyrogram.errors import RPCError
+
+
+def get_text(message: Message) -> [None, str]:
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
 
 
 @Client.on_message(filters.command("eval"))
